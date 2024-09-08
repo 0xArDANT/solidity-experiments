d282840f01511590c84aa85426ca8b3194282354
0xArDANT <aroldtouko94@gmail.com>
2024-09-08 02:06:10 +0100

    New insight: immutable can only be updated inside the constructor

--- Immutable.sol
+++ Immutable.sol
@@ -8,10 +8,15 @@ contract Immutable {
     constructor(uint256 _num) {
         number = _num;
         number = 123;
+        number = 155;
     }
 
-    // After some experiments I come to this conclusion:
-    // The only point I see for using immutable is saving gas by having the value saved directly in the bytecode instead of blockchain storage
-    // It has meaning only if we won't change the value later(it is possible). For example a wrapper contract address, a ERC-20 token address.
+    function updateNumber(uint256 _newNumber) external {
+        number = _newNumber;
+    }
 
+    /* After some experiments I come to this conclusion:
+       The immutable variables can be updated multiple times in the constructor
+       But their value can't be updated inside a function.
+    */
 }
